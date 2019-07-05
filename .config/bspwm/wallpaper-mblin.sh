e1ab25c1f956e0b5f84a8507e2609f6d743a4041
Matt Burrows <m@ttburro.ws>
2019-07-05 09:43:10 +0100

    Added pywal auto theme switching

--- .config/bspwm/wallpaper-mblin.sh
+++ .config/bspwm/wallpaper-mblin.sh
@@ -3,6 +3,7 @@
 xsetroot -solid black
 sleep 5
 while true; do
-    feh --randomize --no-xinerama --bg-fill ~/Pictures/*.jpg
+    #feh --randomize --no-xinerama --bg-fill ~/Pictures/*.jpg
+    wal --vte -q -i ~/Pictures
     sleep 1800
 done
