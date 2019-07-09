15cb269b7a55e53d7fc7a5be873249b0f93b07d2
Matt Burrows <mburrows@batstrading.com>
2019-07-09 15:54:58 +0100

    Added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,47 @@
+# vim: set ft=fish foldmethod=marker:
+
+set -Ux GTAGSFORCECPP 1
+set -Ux EDITOR "emacsclient"
+set -Ux CDPATH . .. ~
+
+# Abbreviations
+abbr -a ga   "git add"
+abbr -a gacm "git add -A . ; git commit --verbose"
+abbr -a gc   "git commit"
+abbr -a gd   "git diff -w"
+abbr -a gdo  "git diff -w origin"
+abbr -a gf   "git pull"
+abbr -a gg   "git --no-pager grep -n --break -i"
+abbr -a gl   "git log"
+abbr -a gls  "git log --oneline"
+abbr -a go   "git checkout"
+abbr -a gp   "git push"
+abbr -a gpum "git pull upstream master"
+abbr -a gr   "git rebase"
+abbr -a grm  "git rebase master"
+abbr -a gs   "git status"
+abbr -a gu   "git add -u"
+abbr -a gv   "git svnup"
+abbr -a gvp  "git svn dcommit"
+abbr -a gz   "git stash"
+
+abbr -a pk   "pkill"
+abbr -a pg   "pgrep -fl"
+
+# Aliases
+alias g="git --no-pager grep -n --break -i"
+alias rl="source ~/.config/fish/config.fish"
+alias tmux="~/local/bin/tmux -2"
+alias e="emacsclient -n"
+alias et="emacsclient -t"
+alias l="ls -lrth"
+
+# Miscellaneous
+eval (dircolors -c ~/.dir_colors)
+
+# Load local configuration (it it exists)
+if test -f ~/.config/fish/(hostname -s).fish
+    source ~/.config/fish/(hostname -s).fish
+end
+
+
---
+++ fish/.config/fish/pcomblin3.fish
@@ -0,0 +1,55 @@
+# BATS specific fish configuration
+
+set -Ux PATH $PATH $HOME/system_tests/scripts
+set -Ux MANPATH $MANPATH $HOME/local/share/man
+
+function compiler_env_clang -d "Set compiler to icecream clang"
+    set -gx ICECC_VERSION $HOME/.icecream/f0e5783327d81cb8424e82c148871f6b.tar.gz
+    set -gx ICECC_CC /opt/bats/bin/clang
+    set -gx ICECC_CXX /opt/bats/bin/clang++
+    set -gx BUILD_DIR /builds/$USER/clang
+    set -gx CC clang
+    set -gx CXX clang++
+end
+
+function compiler_env_gcc -d "Set compiler to icecream gcc"
+    set -gx ICECC_VERSION $HOME/.icecream/22b3d3d5f879cab4c24348e1d977e4b0.tar.gz
+    set -gx ICECC_CC /opt/rh/devtoolset-7/root/usr/bin/gcc
+    set -gx ICECC_CXX /opt/rh/devtoolset-7/root/usr/bin/g++
+    set -gx BUILD_DIR /builds/$USER/gcc
+    set -gx CC gcc
+    set -gx CXX g++
+end
+
+# Default to clang compilation
+compiler_env_clang
+
+set -Ux ECN_ENVIRONMENT {$USER}_beta
+set -Ux ECN_BIN /opt/ecn/users/mburrows/bin
+set -Ux ECN_SRC /opt/ecn/users/mburrows/source/ecn/source
+
+# npm global install locally
+set NPM_PACKAGES {$HOME}/.npm-packages
+set -Ux PATH {$NPM_PACKAGES}/bin $PATH
+
+abbr -a md "~/cpp/bb debug -j32"
+abbr -a mr "~/cpp/bb release -j32"
+abbr -a icemon "env USE_SCHEDULER=pcodev1.uk.bats.com icemon"
+abbr -a ut "$ECN_BIN/ecn_unit_test --show_progress=yes"
+abbr -a utl "$ECN_BIN/ecn_unit_test --log_level=unit_scope"
+abbr -a utlr "$ECN_BIN/ecn_unit_test --log_level=unit_scope --run_test="
+abbr -a utr "$ECN_BIN/ecn_unit_test --show_progress=yes --run_test="
+abbr -a pt "~/cpp/ecn_unit_test/parallel_test -1"
+abbr -a wg "wgrep -P --line-buffered"
+abbr -a wr "wrange -r"
+abbr -a wf "wtail -f"
+abbr -a pw "prt_wire"
+abbr -a pf "prt_fix --nohb"
+abbr -a tf "tail -F"
+abbr -a pks "pkill ecn_ ; pkill mtf_ ; pkill test_orderentry ; pkill test_topclient"
+
+function utg -d "Search for a unit test"
+    eval $ECN_BIN/ecn_unit_test --list | grep -i $argv
+end
+
+
