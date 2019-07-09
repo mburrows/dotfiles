90fbc3a697d5ce048fae24bdad61616a4aa47eac
Matt Burrows <maburrow@googlemail.com>
2019-07-09 22:18:14 +0100

    Made host specific sxhxdrc files.
    Simplified required font list. As part of this the workspace indicators are now
    just dots.

--- bspwm/.config/bspwm/autostart-blade.sh
+++ bspwm/.config/bspwm/autostart-blade.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-setxkbmap us
 xrdb merge ~/.Xresources
 xfsettingsd &
 xfce4-power-manager &
 xautolock -detectsleep -time 10 -locker "i3lock -c 000000" &
 nm-applet &
 compton &
-sxhkd &
+sxhkd -c $HOME/.config/sxhkd/$HOSTNAME-sxhkdrc &
+setxkbmap --layout us
 $HOME/.config/polybar/launch.sh &
--- bspwm/.config/bspwm/autostart-mblin.sh
+++ bspwm/.config/bspwm/autostart-mblin.sh
@@ -4,5 +4,5 @@ xrdb merge ~/.Xresources
 xfsettingsd &
 xautolock -detectsleep -time 10 -locker "i3lock -c 000000" &
 compton &
-sxhkd &
+sxhkd -c $HOME/.config/sxhkd/$HOSTNAME-sxhkdrc &
 $HOME/.config/polybar/launch.sh &
