c66e0503edebe4a6bc3cc66ac2d6b5992e95f4cf
Matt Burrows <m@ttburro.ws>
2019-07-10 12:37:30 +0100

    Changed shortcuts for node flags in sxhkd.
    Added a shortcut for window rotation.

--- bspwm/.config/bspwm/rules-mblin.sh
+++ bspwm/.config/bspwm/rules-mblin.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-bspc rule -a Gimp desktop='^8' state=floating follow=on
 # bspc rule -a Google-chrome desktop='^1' follow=on
-bspc rule -a Emacs desktop='^4' follow=on
+bspc rule -a Emacs desktop='^6' follow=on state=tiled
 bspc rule -a Thunar state=floating
